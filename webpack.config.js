13aa2bed7c8948d1298237b06fa5e6eb470d4db9
maryyak <akovenkomaria557@gmail.com>
2025-07-03 23:40:42 +0300

    Оптимизация

--- webpack.config.js
+++ webpack.config.js
@@ -73,21 +73,6 @@ module.exports = {
                             webp: {
                                 quality: 95,
                                 lossless: true
-                            },
-                            svgo: {
-                                plugins: [
-                                    { name: 'removeViewBox', active: false }, // Не удаляем viewBox
-                                    { name: 'removeDimensions', active: true }, // Удаляем width/height если есть viewBox
-                                    { name: 'cleanupIDs', active: true }, // Удаляем неиспользуемые ID
-                                    { name: 'collapseGroups', active: true }, // Упрощаем группы
-                                    { name: 'removeTitle', active: true }, // Удаляем <title>
-                                    { name: 'removeDesc', active: true }, // Удаляем <desc>
-                                    { name: 'removeUselessDefs', active: true }, // Удаляем неиспользуемые определения
-                                    { name: 'convertPathData', active: true }, // Оптимизируем пути
-                                    { name: 'convertTransform', active: true }, // Оптимизируем трансформации
-                                    { name: 'removeStyleElement', active: true }, // Удаляем стили
-                                    { name: 'removeScriptElement', active: true } // Удаляем скрипты
-                                ]
                             }
                         }
                     }
